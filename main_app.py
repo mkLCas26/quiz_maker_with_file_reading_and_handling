0f8d267139f17bef4683ac22710a42cb01b7eb92
Kristina Cas <m.kristinacas@gmail.com>
2025-04-10 00:24:14 +0800

    Add: collection of username and menu

--- main_app.py
+++ main_app.py
@@ -4,8 +4,8 @@
 import os
 import time
 from pyfiglet import Figlet
-from sample_quiz import run_prequiz
-from user_quiz import input_user_quiz
+# from sample_quiz import run_prequiz
+# from user_quiz import input_user_quiz
 
 # clearing screen
 def clear_content():
@@ -14,5 +14,22 @@ def clear_content():
 # Inserts ASCII art for title
 test = Figlet(font='elite')
 print(test.renderText('~ Quiz Master ~'))
+time.sleep(1.5)
+
+# Ask for username
+username = input("\n\nHi! Enter your username: ")
+clear_content()
+
+# Loop for the controls
+while True: 
+    print(test.renderText('~ Quiz Master ~'))
+    print(f"\n\nWelcome to Quiz Master, {username}")
+    print(f"What would you like to do?")
+    print(f"   [1] Create Quiz (10 Questions)")
+    print(f"   [2] Take Sample Quiz (Random 5 Items Science Quiz)")
+    print(f"   [3] Exit")
+    
+    
+
 
 
