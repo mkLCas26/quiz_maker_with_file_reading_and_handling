5506420cc3f8e48a3b15e41327ff186812443fd2
Kristina Cas <m.kristinacas@gmail.com>
2025-04-10 00:08:12 +0800

    (main_app.py) Add: imported libraries to be used, imported the two files created earlier to integrate in the code, added a program to clear the terminal after tasks, added an ascii art for title of the quiz maker

--- main_app.py
+++ main_app.py
@@ -1,1 +1,18 @@
 # Main Quiz App Program
+
+# import libraries and code files
+import os
+import time
+from pyfiglet import Figlet
+from sample_quiz import run_prequiz
+from user_quiz import input_user_quiz
+
+# clearing screen
+def clear_content():
+    os.system('cls' if os.name == "nt" else "clear")
+
+# Inserts ASCII art for title
+test = Figlet(font='elite')
+print(test.renderText('~ Quiz Master ~'))
+
+
