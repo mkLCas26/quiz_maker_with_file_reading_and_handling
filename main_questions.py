a6d4b464cca1ec3ed917e6f33c796e3b7cb08f18
Kristina Cas <m.kristinacas@gmail.com>
2025-04-09 11:45:55 +0800

    File Added, Added: quiz_pseudocode.txt (contains pseudocode for the whole program) ;  Added Class to main questions

--- main_questions.py
+++ main_questions.py
@@ -1,1 +1,8 @@
 # Main Quiz Logic
+
+class QuizQuestions:
+    def __init__(self, prompt, choices, correct):
+        self.prompt = prompt
+        self.choices = choices
+        self.correct = correct
+
