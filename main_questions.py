f39cee277f81b2d6a9de4824fcbc583e5c25686d
Kristina Cas <m.kristinacas@gmail.com>
2025-04-09 12:14:14 +0800

    Added: 10 questions prompts for the quiz

--- main_questions.py
+++ main_questions.py
@@ -6,3 +6,15 @@ class QuizQuestions:
         self.choices = choices
         self.correct = correct
 
+question_prompts = [
+    "What is the largest ocean in the world?",
+    "What is the deepest trench in the world?",
+    "What is largest gas planet?",
+    "How much of the Earth's surface is water mass?",
+    "What is the second most abundant element in the Earth's atmosphere?",
+    "What is Earth's highest point above sea level?",
+    "How old is Earth?",
+    "What plate is the fastest-moving among all of Earth's plates?",
+    "What planet has the most moons/satelites?",
+    "What are the Earth's four major layers?"
+]
