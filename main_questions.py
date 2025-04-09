d7366eb030fc86b6588f82f99b060f6e25ad3915
Kristina Cas <m.kristinacas@gmail.com>
2025-04-09 12:56:47 +0800

    Remove, Add: Reduced questions into 5 and added list of questions with choices and correct answers

--- main_questions.py
+++ main_questions.py
@@ -1,20 +1,50 @@
 # Main Quiz Logic
 
+# class for quiz questions
 class QuizQuestions:
     def __init__(self, prompt, choices, correct):
         self.prompt = prompt
         self.choices = choices
         self.correct = correct
 
+# list of question prompts
 question_prompts = [
     "What is the largest ocean in the world?",
     "What is the deepest trench in the world?",
     "What is largest gas planet?",
     "How much of the Earth's surface is water mass?",
-    "What is the second most abundant element in the Earth's atmosphere?",
-    "What is Earth's highest point above sea level?",
-    "How old is Earth?",
-    "What plate is the fastest-moving among all of Earth's plates?",
-    "What planet has the most moons/satelites?",
-    "What are the Earth's four major layers?"
+    "What is the second most abundant element in the Earth's atmosphere?"
+]
+
+# list of questions with choices and correct answer index
+quiz_ques = [
+    QuizQuestions(
+        question_prompts[0],
+        ["Pacific Ocean", "Indian Ocean", "Arctic Ocean", "Atlantic Ocean"],
+        0
+    ),
+    
+    QuizQuestions(
+        question_prompts[1],
+        ["Kermadec Trench", "Tonga Trench", "Marianas Trench", "New Britain Trench"],
+        2
+    ),
+    
+    QuizQuestions(
+        question_prompts[2],
+        ["Saturn", "Neptune", "Uranus", "Jupiter"],
+        3
+    ),
+    
+    QuizQuestions(
+        question_prompts[3],
+        ["70%", "71%", "68%", "73%"],
+        1
+    ),
+    
+    QuizQuestions(
+        question_prompts[4],
+        ["Nitrogen", "Argon", "Carbon Dioxide", "Oxygen"],
+        3
+    )
 ]
