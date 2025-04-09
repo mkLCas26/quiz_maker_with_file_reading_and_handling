c0a41d0509735c4a141b2d0e8350a206cc8ce8b6
Kristina Cas <m.kristinacas@gmail.com>
2025-04-09 21:52:27 +0800

    Fix: collection of correct answer per question

--- user_quiz.py
+++ user_quiz.py
@@ -15,16 +15,26 @@ class UseQuestions:
 def input_user_quiz():
     user_question = []
     choices_list = []
+    correct_ans = []
+    choices_choose = "ABCD"
     
     for number in range(1, 11):
+        print("\n--------------------")
         questions = input(f"\nEnter Question {number}: ")
         user_question.append(questions)
         
         for letter in range(4):
             choices = input(f"  Enter Choice {chr(65 + letter)}: ")
             choices_list.append(choices)
+            
+        while True:
+            correct_choice = input(f"\nWhat is the correct answer for this question? ").upper()
+            if correct_choice not in choices_choose:
+                print(f"'{correct_choice}' is invalid. Only choose between A, B, C, and D.")
+            else:
+                correct_ans.append(correct_choice)
+                break
 
-    correct_choice = input(f"What is the correct answer for this question (A/B/C/D)? ").upper()
             
 input_user_quiz()
         
