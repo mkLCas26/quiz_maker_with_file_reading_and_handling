fe9e637c7182a7e41e43098faeb3fc14d31bf65e
Kristina Cas <m.kristinacas@gmail.com>
2025-04-09 21:26:48 +0800

    Add: class for user questions and started the function for user inputs

--- user_quiz.py
+++ user_quiz.py
@@ -1,1 +1,15 @@
 # Main Quiz Logic (for user-made quiz)
+
+# import libraries
+
+# class for quiz questions
+class UseQuestions:
+    def __init__(self, prompt, choices, correct):
+        self.prompt = prompt
+        self.choices = choices
+        self.correct = correct
+
+user_question = []
+
+# function for letting the user add questions
+def input_user_quiz():
