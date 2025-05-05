6bba1a4785d87a3c063b2f9f9103dce6f1ab8162
Kristina Cas <m.kristinacas@gmail.com>
2025-05-05 22:32:34 +0800

    Add: code block that will check if user has created quiz files to choose from the result_folder

--- answer_user_quiz.py
+++ answer_user_quiz.py
@@ -4,3 +4,17 @@
 import os
 import random
 
+# function that allows user to choose a quiz file from folder
+def list_avail_quizzes():
+    quizzes = []
+    
+    # checks if result_folder contains quiz files
+    for file in os.listdir("result_folder"):
+        if file.endswith(".txt"):
+            quizzes.append(file)
+    
+    if not quizzes:
+        print("\nThere's no quizzes created yet.")
+        return None
+    
+    
