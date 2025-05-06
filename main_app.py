d603effeeb142b7f799e3446858e75dd0173d4db
Kristina Cas <m.kristinacas@gmail.com>
2025-05-07 00:35:03 +0800

    Design: added colorama library to add colors to main_app

--- main_app.py
+++ main_app.py
@@ -4,9 +4,14 @@
 import os
 import time
 from pyfiglet import Figlet
+from colorama import Fore, Back, Style, init
+
 from sample_quiz import run_prequiz, sample_ques
 from user_quiz import input_user_quiz
 from answer_user_quiz import answer_selected_quiz
+
+# initialize colorama
+init(autoreset=True)
 
 # function for clearing screen 
 def clear_content():
@@ -14,53 +19,53 @@ def clear_content():
 
 # Inserts ASCII art for title
 test = Figlet(font='elite')
-clear_content()
-print(test.renderText('~ Quiz Master ~'))
+clear_content() 
+print(Fore.YELLOW + test.renderText('~ Quiz Master ~'))
 time.sleep(1)
 
 # enter username
-username = input("\n\nHi! Enter your username: ")
+username = input("\n\nHi! Enter your username 👾 : ")
 clear_content()
 
 # Loop for the controls
 while True: 
     clear_content()
-    print(test.renderText('~ Quiz Master ~'))
-    print(f"\n\nWelcome to Quiz Master, {username}")
-    print(f"What would you like to do?")
-    print(f"   [1] Create Quiz (10 Questions)")
-    print(f"   [2] Take Sample Quiz (Random 5 Items Science Quiz)")
-    print(f"   [3] Answer Your Created Quiz")
-    print(f"   [4] Exit")
-    
-    selected = input("Select an option (1-4): ")
+    print(Fore.YELLOW + test.renderText('~ Quiz Master ~'))
+    print(f"\n\n{Fore.WHITE}Welcome to Quiz Master ✨, {Fore.GREEN + username}{Fore.WHITE}! Let's get started 🔥")
+    print(f"\n{Fore.YELLOW}What would you like to do? 🤸")
+    print(f"{Fore.CYAN}   [1] 📝 Create Quiz (10 Questions)")
+    print(f"{Fore.CYAN}   [2] 🧩 Take Sample Quiz (Random 5 Items Science Quiz)")
+    print(f"{Fore.CYAN}   [3] 🏅 Answer Your Created Quiz")
+    print(f"{Fore.RED}   [4] 😭 Exit")
+
+    selected = input(f"{Fore.GREEN}Select an option (1-4): ")
     
     if selected == "1":
         clear_content()
-        print(test.renderText('~ Quiz Master ~'))
+        print(Fore.CYAN + test.renderText('~ Quiz Master ~'))
         input_user_quiz()
-        input("\n\nPress Enter to return to Main Menu")
+        input(f"\n\n{Back.CYAN + Style.DIM}Press Enter to return to Main Menu 🤸 ...{Style.RESET_ALL}")
         clear_content()
     
     elif selected == "2":
         clear_content()
-        print(test.renderText('~ Quiz Master ~'))
+        print(Fore.CYAN + test.renderText('~ Quiz Master ~'))
         run_prequiz(sample_ques)
-        input("\n\nPress Enter to return to Main Menu")
+        input(f"\n\n{Back.CYAN + Style.DIM}Press Enter to return to Main Menu 🤸 ...{Style.RESET_ALL}")
         clear_content()
         
     elif selected == "3":
         clear_content()
-        print(test.renderText('~ Quiz Master ~'))
+        print(Fore.CYAN + test.renderText('~ Quiz Master ~'))
         answer_selected_quiz()
-        input("\n\nPress Enter to return to Main Menu")
+        input(f"\n\n{Back.CYAN + Style.DIM}Press Enter to return to Main Menu 🤸 ...{Style.RESET_ALL}")
         clear_content()
     
     elif selected == "4":
-        print(f"\n\nAww! Thank you for playing, see you soon {username}!")
+        print(f"\n\n{Fore.YELLOW}Aww! Thank you for playing, see you soon {Fore.GREEN + username}!")
         break
     
     else:
-        print("Invalid! Only select from 1-4. Kindly try again.")
+        print(f"{Fore.RED}Invalid! Only select from 1-4. Kindly try again.")
         time.sleep(0.5)
         clear_content()
